4376fa57496cde4b10603dfdadbcae8ee7887d98
Sakshi330g <sakshibhagat330@gmail.com>
2026-03-28 07:46:23 +0530

    finale

--- src/environment.js
+++ src/environment.js
@@ -1,8 +1,8 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-     "https://backend-9pnl.onrender.com" :
+    "https://backend-9pnl.onrender.com" :
 
-     "http://localhost:8000"
+    "http://localhost:8000"
 
 
 export default server;
